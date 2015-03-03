5a198d0d1da2ddb707bdf93891064685dbcd89fe
Matheus Claudino <matheusclaudin@gmail.com>
2015-03-03 15:51:45 -0300

    Componentes e layout adicionados ao projeto

--- exercicio-02-media-player/Unit1.pas
+++ exercicio-02-media-player/Unit1.pas
@@ -4,10 +4,35 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, ExtCtrls, MPlayer, Menus, Buttons, ComCtrls, StdCtrls;
 
 type
   TForm1 = class(TForm)
+    PButtons: TPanel;
+    ListBox1: TListBox;
+    PPlayer: TPanel;
+    ProgressBar1: TProgressBar;
+    StatusBar1: TStatusBar;
+    SBPlay: TSpeedButton;
+    SBPause: TSpeedButton;
+    SBStop: TSpeedButton;
+    SBNext: TSpeedButton;
+    SBPrior: TSpeedButton;
+    SBCima: TSpeedButton;
+    SBBaixo: TSpeedButton;
+    SBDelete: TSpeedButton;
+    MainMenu1: TMainMenu;
+    Arquivo1: TMenuItem;
+    Abrir1: TMenuItem;
+    Opes1: TMenuItem;
+    RepetirMsica1: TMenuItem;
+    Ativar1: TMenuItem;
+    Desativar1: TMenuItem;
+    MediaPlayer1: TMediaPlayer;
+    OpenDialog1: TOpenDialog;
+    Timer1: TTimer;
+    Timer2: TTimer;
+    Timer3: TTimer;
   private
     { Private declarations }
   public
@@ -21,5 +46,5 @@ implementation
 
 {$R *.dfm}
 
+
 end.
- 
