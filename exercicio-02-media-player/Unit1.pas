27d02e896aaf8c14da84a8b8b21ac2f03c53c540
Matheus Claudino <matheusclaudin@gmail.com>
2015-03-03 16:14:45 -0300

    Eventos de click adicionados com suas respectivas funcionalidades aos SpeedButtons: Play, Pause, Stop, Next e Prior

--- exercicio-02-media-player/Unit1.pas
+++ exercicio-02-media-player/Unit1.pas
@@ -35,6 +35,11 @@ type
     Timer3: TTimer;
     procedure Abrir1Click(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
+    procedure SBPlayClick(Sender: TObject);
+    procedure SBPauseClick(Sender: TObject);
+    procedure SBStopClick(Sender: TObject);
+    procedure SBNextClick(Sender: TObject);
+    procedure SBPriorClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -70,6 +75,71 @@ end;
 procedure TForm1.Timer1Timer(Sender: TObject);
 begin
   ProgressBar1.Position:= MediaPlayer1.Position;
+end;
+
+procedure TForm1.SBPlayClick(Sender: TObject);
+begin
+  MediaPlayer1.FileName:= ListBox1.Items.Strings[ListBox1.ItemIndex];
+  StatusBar1.SimpleText:= ExtractFileName(MediaPlayer1.FileName);
+  MediaPlayer1.Open;
+  MediaPlayer1.Play;
+end;
+
+procedure TForm1.SBPauseClick(Sender: TObject);
+begin
+  MediaPlayer1.Pause;
+end;
+
+procedure TForm1.SBStopClick(Sender: TObject);
+begin
+  MediaPlayer1.FileName:= ListBox1.Items.Strings[ListBox1.ItemIndex];
+  StatusBar1.SimpleText:= ExtractFileName(MediaPlayer1.FileName);
+  MediaPlayer1.Open;
+  MediaPlayer1.Stop;
+end;
+
+procedure TForm1.SBNextClick(Sender: TObject);
+begin
+  if (ListBox1.ItemIndex < ListBox1.Items.Count -1) then
+        begin
+          ListBox1.ItemIndex:= ListBox1.ItemIndex +1;
+          MediaPlayer1.FileName:= ListBox1.Items.Strings[ListBox1.ItemIndex];
+          StatusBar1.SimpleText:= ExtractFileName(MediaPlayer1.FileName);
+          MediaPlayer1.Open;
+          ProgressBar1.Max := MediaPlayer1.Length;
+          MediaPlayer1.Play;
+        end
+      else
+        begin
+          ListBox1.ItemIndex:= 0;
+          MediaPlayer1.FileName:= ListBox1.Items.Strings[ListBox1.ItemIndex];
+          StatusBar1.SimpleText:= ExtractFileName(MediaPlayer1.FileName);
+          MediaPlayer1.Open;
+          ProgressBar1.Max := MediaPlayer1.Length;
+          MediaPlayer1.Play;
+        end;
+end;
+
+procedure TForm1.SBPriorClick(Sender: TObject);
+begin
+  if (ListBox1.ItemIndex > 0) then
+    begin
+      ListBox1.ItemIndex:= ListBox1.ItemIndex -1;
+      MediaPlayer1.FileName:= ListBox1.Items.Strings[ListBox1.ItemIndex];
+      StatusBar1.SimpleText:= ExtractFileName(MediaPlayer1.FileName);
+      MediaPlayer1.Open;
+      ProgressBar1.Max := MediaPlayer1.Length;
+      MediaPlayer1.Play;
+    end
+  else
+    begin
+      ListBox1.ItemIndex:= ListBox1.Items.Count -1;
+      MediaPlayer1.FileName:= ListBox1.Items.Strings[ListBox1.ItemIndex];
+      StatusBar1.SimpleText:= ExtractFileName(MediaPlayer1.FileName);
+      MediaPlayer1.Open;
+      ProgressBar1.Max := MediaPlayer1.Length;
+      MediaPlayer1.Play;
+    end;
 end;
 
 end.
